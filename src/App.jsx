4d9abcffff50e26302f155b7cb0530619b42ed4c
Maja <majanikoljic@gmail.com>
2025-07-08 12:33:08 -0400

    Implement Coffee Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 import gsap from 'gsap';
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
+import Coffees from "./components/coffees.jsx";
 
 const App = () => {
     return (
         <main>
             <Navbar />
             <Hero />
+            <Coffees />
         </main>
     )
 }
---
+++ src/components/coffees.jsx
@@ -0,0 +1,68 @@
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
+import {hotDrinks, icedDrinks} from "../../constants/index.js";
+
+
+const Coffees = () => {
+    useGSAP(() => {
+        const parallaxTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: '#coffee',
+                start: 'top 30%',
+                end: 'bottom 80%',
+                scrub: true,
+            }
+        })
+
+        parallaxTimeline
+            .from('#c-left-leaf', {
+                x: -100, y: 100
+            })
+            .from('#c-right-leaf', {
+                x: 100, y: 100
+            })
+    })
+
+    return (
+        <section id="coffee" className="noisy">
+            <img src="/images/coffee-left-leaf.png" alt="l-leaf" id="c-left-leaf"/>
+            <img src="/images/coffee-right-leaf.png" alt="r-leaf" id="c-right-leaf"/>
+
+            <div className="list">
+                <div className="hot">
+                    <h2>Hot Coffees:</h2>
+
+                    <ul>
+                        {hotDrinks.map(({name, origin, detail, price}) => (
+                            <li key={name}>
+                                <div className="md:me-28">
+                                    <h3>{name}</h3>
+                                    <p>{origin} | {detail}</p>
+                                </div>
+                                <span>- {price}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className="iced">
+                    <h2>Iced Coffees:</h2>
+
+                    <ul>
+                        {icedDrinks.map(({name, origin, detail, price}) => (
+                            <li key={name}>
+                                <div className="me-28">
+                                    <h3>{name}</h3>
+                                    <p>{origin} | {detail}</p>
+                                </div>
+                                <span>- {price}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </section>
+
+    )
+}
+export default Coffees;
